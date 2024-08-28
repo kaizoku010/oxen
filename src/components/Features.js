582a1c7a5ce8a37a5e5e7e4d19d0f0c7f3f88279
mdixon <mdixon@root.kaizoku>
2024-08-28 07:20:38 -0500

    oxen impact designs

---
+++ src/components/Features.js
@@ -0,0 +1,282 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import Chip from '@mui/material/Chip';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import DevicesRoundedIcon from '@mui/icons-material/Adjust';
+import EdgesensorHighRoundedIcon from '@mui/icons-material/Visibility';
+import ViewQuiltRoundedIcon from '@mui/icons-material/JoinInner';
+import AfricanLady from "../media/hands.jpg"
+import Vision from "../media/vision.jpeg"
+import Mission from "../media/mission2.jpeg"
+import Core from "../media/core.jpg"
+import Mobile from "../mobile.css"
+
+
+const imgLink="https://img.freepik.com/free-photo/business-hands-joined-together-teamwork_53876-30575.jpg?t=st=1724834a59~exp=1724837959~hmac=30c3898f08efda265a3eaae06ca649f079c687a33144cd4d71d8ddc0b7b91a76&w=1800"
+
+const items = [
+  {
+    icon: <ViewQuiltRoundedIcon />,
+    title: 'Core values',
+    description:
+      'This item could provide a snapshot of the most important metrics or data points related to the product.',
+      imageLight: `url(${Vision})`,
+      imageDark: `url(${Vision})`, // Use the imported image or URL directly    imageDark: imgLink,
+  },
+  {
+    icon: <EdgesensorHighRoundedIcon />,
+    title: 'Vision',
+    description:
+      'This item could provide information about the mobile app version of the product.',
+      imageLight: `url(${Mission})`,
+      imageDark: `url(${Mission})`, // Use the imported image or URL directly    imageDark: imgLink,
+  },
+  {
+    icon: <DevicesRoundedIcon />,
+    title: 'Mission',
+    description:
+      'This item could let users know the product is available on all platforms, such as web, mobile, and desktop.',
+      imageLight: `url(${AfricanLady})`,
+      imageDark: `url(${imgLink})`, // Use the imported image or URL directly    imageDark: imgLink,
+  },
+];
+
+export default function Features() {
+  const [selectedItemIndex, setSelectedItemIndex] = React.useState(0);
+
+  const handleItemClick = (index) => {
+    setSelectedItemIndex(index);
+  };
+
+  const selectedFeature = items[selectedItemIndex];
+
+  return (
+    <Container id="features" sx={{ py: { xs: 8, sm: 16 } }}>
+      <Grid container spacing={6}>
+        <Grid item xs={12} md={6}>
+          <div>
+            <Typography component="h2" variant="h4" color="text.primary">
+            More about us
+            </Typography>
+            <Typography
+              variant="body1"
+              className='more_text'
+              color="text.secondary"
+              sx={{ mb: { xs: 2, sm: 4 } }}
+            >
+              Here you can provide a brief overview of the key features of the
+              product. For example, you could list the number of features, the types
+              of features, add-ons, or the benefits of the features.
+            </Typography>
+          </div>
+          <Grid className='chip_grid' container item gap={1} sx={{ display: { xs: 'auto', sm: 'none' } }}>
+            {items.map(({ title }, index) => (
+              <Chip
+                key={index}
+                label={title}
+                onClick={() => handleItemClick(index)}
+                sx={{
+                  borderColor: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index ? 'primary.light' : '';
+                    }
+                    return selectedItemIndex === index ? 'primary.light' : '';
+                  },
+                  background: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index ? 'none' : '';
+                    }
+                    return selectedItemIndex === index ? 'none' : '';
+                  },
+                  backgroundColor: selectedItemIndex === index ? 'primary.main' : '',
+                  '& .MuiChip-label': {
+                    color: selectedItemIndex === index ? '#fff' : '',
+                  },
+                }}
+              />
+            ))}
+          </Grid>
+          <Box
+            component={Card}
+            variant="outlined"
+            sx={{
+              display: { xs: 'auto', sm: 'none' },
+              mt: 4,
+            }}
+          >
+            <Box
+              sx={{
+                backgroundImage: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? items[selectedItemIndex].imageLight
+                    : items[selectedItemIndex].imageDark,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                minHeight: 280,
+              }}
+            />
+            <Box sx={{ px: 2, pb: 2 }}>
+              <Typography  className='ab_title' color="text.primary" variant="body2" fontWeight="bold">
+                {selectedFeature.title}
+              </Typography>
+              <Typography color="text.secondary" variant="body2" sx={{ my: 0.5 }}>
+                {selectedFeature.description}
+              </Typography>
+              <Link
+                color="primary"
+                variant="body2"
+                fontWeight="bold"
+                sx={{
+                  display: 'inline-flex',
+                  alignItems: 'center',
+                  '& > svg': { transition: '0.2s' },
+                  '&:hover > svg': { transform: 'translateX(2px)' },
+                }}
+              >
+                <span>Learn more</span>
+                <ChevronRightRoundedIcon
+                  fontSize="small"
+                  sx={{ mt: '1px', ml: '2px' }}
+                />
+              </Link>
+            </Box>
+          </Box>
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="flex-start"
+            spacing={2}
+            useFlexGap
+            sx={{ width: '100%', display: { xs: 'none', sm: 'flex' } }}
+          >
+            {items.map(({ icon, title, description }, index) => (
+              <Card
+                key={index}
+                variant="outlined"
+                component={Button}
+                onClick={() => handleItemClick(index)}
+                sx={{
+                  p: 3,
+                  height: 'fit-content',
+                  width: '100%',
+                  background: 'none',
+                  backgroundColor:
+                    selectedItemIndex === index ? 'action.selected' : undefined,
+                  borderColor: (theme) => {
+                    if (theme.palette.mode === 'light') {
+                      return selectedItemIndex === index
+                        ? 'primary.light'
+                        : 'grey.200';
+                    }
+                    return selectedItemIndex === index ? 'primary.dark' : 'grey.800';
+                  },
+                }}
+              >
+                <Box
+                  sx={{
+                    width: '100%',
+                    display: 'flex',
+                    textAlign: 'left',
+                    flexDirection: { xs: 'column', md: 'row' },
+                    alignItems: { md: 'center' },
+                    gap: 2.5,
+                  }}
+                >
+                  <Box
+                    sx={{
+                      color: (theme) => {
+                        if (theme.palette.mode === 'light') {
+                          return selectedItemIndex === index
+                            ? 'primary.main'
+                            : 'grey.300';
+                        }
+                        return selectedItemIndex === index
+                          ? 'primary.main'
+                          : 'grey.700';
+                      },
+                    }}
+                  >
+                    {icon}
+                  </Box>
+                  <Box sx={{ textTransform: 'none' }}>
+                    <Typography
+                      color="text.primary"
+                      variant="body2"
+                      fontWeight="bold"
+                    >
+                      {title}
+                    </Typography>
+                    <Typography
+                      color="text.secondary"
+                      variant="body2"
+                      sx={{ my: 0.5 }}
+                    >
+                      {description}
+                    </Typography>
+                    <Link
+                      color="primary"
+                      variant="body2"
+                      fontWeight="bold"
+                      sx={{
+                        display: 'inline-flex',
+                        alignItems: 'center',
+                        '& > svg': { transition: '0.2s' },
+                        '&:hover > svg': { transform: 'translateX(2px)' },
+                      }}
+                      onClick={(event) => {
+                        event.stopPropagation();
+                      }}
+                    >
+                      <span>Learn more</span>
+                      <ChevronRightRoundedIcon
+                        fontSize="small"
+                        sx={{ mt: '1px', ml: '2px' }}
+                      />
+                    </Link>
+                  </Box>
+                </Box>
+              </Card>
+            ))}
+          </Stack>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{ display: { xs: 'none', sm: 'flex' }, width: '100%' }}
+        >
+          <Card
+            variant="outlined"
+            sx={{
+              height: '100%',
+              width: '100%',
+              display: { xs: 'none', sm: 'flex' },
+              pointerEvents: 'none',
+            }}
+          >
+            <Box
+              sx={{
+                m: 'auto',
+                width: 420,
+                backgroundPosition:"center",
+                height: 500,
+                backgroundSize: 'cover',
+                backgroundImage: (theme) =>
+                  theme.palette.mode === 'light'
+                    ? items[selectedItemIndex].imageLight
+                    : items[selectedItemIndex].imageDark,
+              }}
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
---
+++ src/components/Hero.js
@@ -0,0 +1,116 @@
+import * as React from 'react';
+import { alpha } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import AfricanLady from "../media/aa.jpg";
+import "./hero.css"
+
+export default function Hero() {
+  return (
+    <Box
+      id="hero"
+      sx={(theme) => ({
+        width: '100%',
+        backgroundImage:
+          theme.palette.mode === 'light'
+            ? 'linear-gradient(180deg, #C4D7AF, #FFF)'
+            : `linear-gradient(#02294F, ${alpha('#090E10', 0.0)})`,
+        backgroundSize: '100% 20%',
+        backgroundRepeat: 'no-repeat',
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+          position: 'relative', // Added for positioning the floating div
+        }}
+      >
+        <Stack spacing={2} useFlexGap sx={{ width: { xs: '100%', sm: '70%' } }}>
+          <Typography
+            variant="h1"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', md: 'row' },
+              alignSelf: 'center',
+              textAlign: 'center',
+              fontSize: 'clamp(3.5rem, 10vw, 4rem)',
+            }}
+          >
+            Oxen impact&nbsp;
+            <Typography
+              component="span"
+              variant="h1"
+              sx={{
+                fontSize: 'clamp(3rem, 10vw, 4rem)', 
+                color: (theme) =>
+                  theme.palette.mode === 'light' ? 'primary.main' : 'primary.light',
+              }}
+            >
+              designs
+            </Typography>
+          </Typography>
+          <Typography
+            textAlign="center"
+            color="text.secondary"
+            sx={{ alignSelf: 'center', width: { sm: '100%', md: '80%' } }}
+          >
+            Explore our cutting-edge collection, delivering high-quality products
+            tailored to your style. Elevate your experience with top-tier products
+            and services.
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            alignSelf="center"
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: 'auto' } }}
+          >
+            <TextField
+              id="outlined-basic"
+              hiddenLabel
+              size="small"
+              variant="outlined"
+              aria-label="Enter your email address"
+              placeholder="Your email address"
+              inputProps={{
+                autoComplete: 'off',
+                'aria-label': 'Enter your email address',
+              }}
+            />
+            <Button variant="contained" color="primary">
+              Reach out
+            </Button>
+          </Stack>
+          <Typography variant="caption" textAlign="center" sx={{ opacity: 0.8 }}>
+            By clicking &quot;Reach out&quot; you agree to our&nbsp;
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+            .
+          </Typography>
+        </Stack>
+<div className='hero'>
+<div className='afroGlobalHolder2'></div>
+
+<div className='afroGlobalHolder'>
+  <h2 className='ox_title'> AfroGlobal Styles</h2>
+  <p className='ox_exp_text'>Being proudly AfroGlobal is not only a fashion style, but its also about celebrating and embracing a global identity that embraces African cultural heritage and creativity and contribute to a more connected and positive world. Proudly</p>
+<button
+id="mdx"
+class="MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium MuiButton-colorPrimary MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeMedium MuiButton-containedSizeMedium btn MuiButton-colorPrimary css-1sf2tdl-MuiButtonBase-root-MuiButton-root">
+  Visit Shop</button>
+</div>
+</div>
+      </Container>
+    </Box>
+  );
+}
